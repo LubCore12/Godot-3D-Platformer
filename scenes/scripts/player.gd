03fac9172db1ab1197fe057ab55d4100f94916d6
Andrew Fomin <andrfom1234567810@gmail.com>
2026-08-05 09:55:11 +0700

    Added basic movement

---
+++ scenes/scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody3D
+
+var direction: Vector2
+
+@export_group("Movement")
+@export var speed: float
+
+func _physics_process(_delta: float) -> void:
+	get_input()
+	velocity.x = direction.x * speed 
+	velocity.z = direction.y * speed
+	move_and_slide()
+	
+func get_input() -> void:
+	direction = Input.get_vector("left", "right", "forward", "backward")
